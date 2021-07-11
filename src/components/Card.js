18f02d9181a51fb63dded84655ae2283a053b40a
IhtishamMushtaq <musaibmushtaqq@gmail.com>
2021-07-11 20:25:16 +0530

    added functionality and initial styles

---
+++ src/components/Card.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useGlobalContext } from "../context/context";
+import "../components/styles.css"
+
+function Card({ id, name, img_url, description, isVeg, price, rating }) {
+  const { dispatch } = useGlobalContext();
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      payload: { id, name, img_url, description, isVeg, price, rating },
+    });
+    // alert("item added to cart");
+  };
+
+  return (
+    <div className="card">
+      <h1>{name}</h1>
+      <img src={img_url} alt={name} width="200px" />
+      <p>{description}</p>
+      <h4>Type : {isVeg ? "Veg" : "Non Veg"}</h4>
+      <h4>Price: INR {price} /-</h4>
+      <h4>Rating: {rating}/5</h4>
+      <button onClick={addToBasket}>Add to cart</button>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import Card from "../components/Card";
+import Header from "../components/Header";
+import "../components/styles.css"
+import img from "../components/images/filter.png"
+
+let clickCount=0;
+function Dashboard() {
+  const [pizzaList, setPizzaList] = useState([]);
+  const [cpyPizza, setCpyPizz] = useState([]);
+  const copy=[...pizzaList];
+  
+ 
+const displayFilters=()=>{
+  if(clickCount==0)
+{  document.getElementById("Button-wrap").style.left="0%";
+clickCount=1;
+}
+else{
+  document.getElementById("Button-wrap").style.left="-100%";
+  clickCount=0;
+}
+
+}
+
+  const sortPizzaList = (id) => {
+    let sortedList;
+    let newPizzArr;
+    if (id === "price") {
+      // sortedList = pizzaList.sort((a, b) => (a.price > b.price ? 1 : -1));
+      sortedList=copy.sort((a, b) => (a.price > b.price ? 1 : -1));
+      setPizzaList(copy);
+    } else if (id === "rating") {
+      sortedList = copy.sort((a, b) => (a.rating < b.rating ? 1 : -1));
+      setPizzaList(copy);
+      
+    } else if (id === "veg") {
+      newPizzArr = cpyPizza.filter((item) => item.isVeg);
+      setPizzaList(newPizzArr);
+    } else if (id === "nonVeg") {
+      newPizzArr = cpyPizza.filter((item) => !item.isVeg);
+      setPizzaList(newPizzArr);
+    } else if (id === "all") {
+      setPizzaList(cpyPizza);
+    }
+    
+  };
+
+  useEffect(() => {
+    fetch("https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68")
+      .then((res) => res.json())
+      .then((data) => {
+        setPizzaList(data);
+        setCpyPizz(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <>
+      <Header />
+      <h2 id="headerIntro">What We Offer</h2>
+      <nav id="filterHead">Sort</nav>
+      <div id="Button-wrap" className="btn-wrapper">
+        <button onClick={() => sortPizzaList("all")}>All</button>
+        <button onClick={() => sortPizzaList("veg")}>Veg</button>
+        <button onClick={() => sortPizzaList("nonVeg")}>Non Veg</button>
+        <button onClick={() => sortPizzaList("price")}>Price</button>
+        <button onClick={() => sortPizzaList("rating")}>Rating</button>
+        </div>
+      <div className="card-container">
+        {pizzaList.map((pizza) => (
+          <Card key={pizza.id} {...pizza} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
